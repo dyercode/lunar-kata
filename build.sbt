c47c097805dccdd94f6800bb6555814d655c9982
Jon <dyercode@users.noreply.github.com>
2022-11-03 14:24:09 -0400

    version bumps.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ import Dependencies._
 
 name := "lunar-kata"
 
-scalaVersion := "3.0.0"
+scalaVersion := "3.2.1"
 
 version := "0.1"
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   // Versions
 
   // Libraries
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
 
   // Projects
   val lunarDependencies = Seq(scalaTest % Test)
